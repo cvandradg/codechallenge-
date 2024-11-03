e5812586d89981f1da36a0c77bf3b26985d772ff
claudio <54946311+cvandradg@users.noreply.github.com>
2024-11-02 21:30:00 -0600

    feat: correct hosting and fb app config

--- libs/testapp/frontend/ui/app/src/lib/lib.routes.ts
+++ libs/testapp/frontend/ui/app/src/lib/lib.routes.ts
@@ -1,4 +1,7 @@
 import { Route } from '@angular/router';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '@testapp/shared/environments/environment';
 import { TestappFrontendAppComponent } from './testapp-frontend-app/testapp-frontend-app.component';
 
 export const testappFrontendAppRoutes: Route[] = [
@@ -6,6 +9,10 @@ export const testappFrontendAppRoutes: Route[] = [
     path: '',
     pathMatch: 'prefix',
     component: TestappFrontendAppComponent,
+    providers: [
+      provideAuth(() => getAuth()),
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    ],
     children: [
       {
         path: '',
---
+++ libs/testapp/frontend/ui/shared/src/lib/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCKcB8pJ4qFXsjqiwGo5itqVfXQImYKd5g',
+    authDomain: 'test-challenge-fb.firebaseapp.com',
+    projectId: 'test-challenge-fb',
+    storageBucket: 'test-challenge-fb.firebasestorage.app',
+    messagingSenderId: '1095682872037',
+    appId: '1:1095682872037:web:883c04b569843d064bfa0c',
+    measurementId: 'G-GDFG9Y098Z',
+  },
+  production: false,
+};
