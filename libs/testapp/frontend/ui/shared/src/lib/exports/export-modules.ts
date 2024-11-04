092c9eae0258aa4e451b6db36618f2f41b739703
claudio <54946311+cvandradg@users.noreply.github.com>
2024-11-04 13:56:10 -0600

    feat: functional list display

--- libs/testapp/frontend/ui/shared/src/lib/exports/export-modules.ts
+++ libs/testapp/frontend/ui/shared/src/lib/exports/export-modules.ts
@@ -1,4 +1,4 @@
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,10 +12,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
@@ -25,7 +24,6 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LetDirective, PushPipe } from '@ngrx/component';
-import { HttpClient } from '@angular/common/http';
 
 export const MODULES = [
   PushPipe,
@@ -55,4 +53,5 @@ export const MODULES = [
   ReactiveFormsModule,
   MatSlideToggleModule,
   MatAutocompleteModule,
+  MatProgressSpinnerModule,
 ];
